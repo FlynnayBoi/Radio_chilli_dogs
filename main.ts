13ed122fff233fe03f1adfa723a37735d029c6b0
FlynnayBoi <78829192+FlynnayBoi@users.noreply.github.com>
2021-02-15 09:04:46 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("CHILLI DOGS")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(1e+308)
+basic.forever(function () {
+	
+})
